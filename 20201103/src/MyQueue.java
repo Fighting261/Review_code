deb5c8f0ec7b0ce8fedd95c522fe08fb478b50d3
Fighting261 <1227873949@qq.com>
2020-11-03 22:16:23 +0800

    栈实现队列

---
+++ 20201103/src/MyQueue.java
@@ -0,0 +1,50 @@
+import java.util.Stack;
+//两个栈实现队列
+class MyQueue {
+    private Stack<Integer> stack1;
+    private Stack<Integer> stack2;
+    /** Initialize your data structure here. */
+    public MyQueue() {
+        stack1 = new Stack<>();
+        stack2 = new Stack<>();
+    }
+
+    /** Push element x to the back of queue. */
+    public void push(int x) {
+        stack1.push(x);
+    }
+
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+        if(empty()){
+            return -1;
+        }
+        if(stack2.empty()){
+            while(!stack1.empty()){
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.pop();
+    }
+
+    /** Get the front element. */
+    public int peek() {
+        if(empty()){
+            return -1;
+        }
+        if(stack2.empty()){
+            while(!stack1.empty()){
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.peek();
+    }
+
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+        if(stack1.empty() && stack2.empty()){
+            return true;
+        }
+        return false;
+    }
+}
